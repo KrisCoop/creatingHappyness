da96ef270d31f372e1140448bdbf6bb528e27859
KrisCoop <47994388+KrisCoop@users.noreply.github.com>
2019-07-01 09:40:31 -0600

    trying anything at this point

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.get('/messages', (req, res) => {
 /// Catch all for routing. Must be below all other routes. 
 app.get('/*', (req, res) => {
     res.sendFile('index.html', {
-        root: path.join(__dirname, "build")
+        root: path.join(__dirname, 'build')
       })
 });
 
